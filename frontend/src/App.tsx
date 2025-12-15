f7553aee337f23123a626f82d48245cdf14b48c8
wellingtonlimaa <well.oliveira1313@gmail.com>
2025-12-15 18:48:51 -0300

    feat: adiciona navbar, botões globais e preview de templates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
-import './styles/App.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MyButtonAlert from "./componente/MyButton";
+import MessagePreview from "./componente/MessagePreview"
+import Navbar from "./componente/global/Navbar"
+import  "./styles/App.css"  
 
 function App() {
-
   return (
+    
     <>
-     
+      <Navbar />
+      {/* CONTAINER GLOBAL */}
+      <div className="ContentButton">
+      <ToastContainer />
+      <MyButtonAlert variant="success">Enviar</MyButtonAlert>
+      <MyButtonAlert variant="danger">Cancelar</MyButtonAlert>
+      </div>
+      <MessagePreview />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
