13516ef3ab310738a8f30f6f0ba10c88c785a7a3
Lucas Santos <lucas.santos.contatoempresarial@gmail.com>
2025-12-18 13:17:30 -0300

    style: alteração nos inputs, e  estilo do botão de importar contatos

--- frontend/src/componente/importar-contatos.tsx
+++ frontend/src/componente/importar-contatos.tsx
@@ -3,23 +3,26 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import '../styles/importar-contatos.css';
 import type { MyInputProps } from '../types';
 
-export default function InputFileUpload({label, style}: MyInputProps) {
+export default function InputFileUpload({ label, style }: MyInputProps) {
   return (
     <>
       {label && <label style={style}>{label}</label>}
+
       <Button
-        className="importarContatosButton"
+        className="importarContatosButton  "
         component="label"
-        startIcon={<CloudUploadIcon/>}
-        
+        startIcon={<CloudUploadIcon />}
       >
-        <input
+        Escolher arquivos
+        <input 
           type="file"
           multiple
-          className="importarContatosInput"
+          hidden   
           onChange={(event) => console.log(event.target.files)}
         />
-      </Button>
+      </Button >
+      
     </>
   );
 }
+
