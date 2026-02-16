d349df8e08bfac18dc0d6d05a96488fad9b947dd
Lucas Santos <lucas.santos.contatoempresarial@gmail.com>
2026-02-16 09:24:55 -0300

    feat/frontend: Criando a página de campanhas (adicionando a lógica do account automático, necessário ajsutar para ficar dinâmico)

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,21 +1,31 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
+
 import EfetuarDisparo from "../pages/EfetuarDisparo/EfetuarDisparo";
 import Templates from "../pages/TemplatesMeta/TemplatesMeta";
-// import HistoricoDisparo from "../pages/historico-disparo";
-import {HistoricoDisparoPage} from "../pages/Historico/historico-disparo";
+import { HistoricoDisparoPage } from "../pages/Historico/historico-disparo";
 import { ClientesVencidos } from "../pages/ClientesVencidos/ClientesVencidos";
 import CreateTemplate from "../pages/TemplatesMeta/Subpages/CreateTemplate";
-
-
+import Campanhas from "../pages/Campanhas/Subpages/Campanhas";
+import { AccountLayout } from "./AccountLayout";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<EfetuarDisparo />} />
-      <Route path="/templates" element={<Templates />} />
-      <Route path="/historico" element={<HistoricoDisparoPage />} />
-      <Route path="/clientesVencidos" element={<ClientesVencidos/>} />
-      <Route path="/CreateTemplate" element={<CreateTemplate />} />
+
+      {/* ROTA RAIZ (recebe account e distribui) */}
+      <Route path="/" element={<AccountLayout />}>
+
+        {/* página inicial */}
+        <Route index element={<EfetuarDisparo />} />
+
+        <Route path="templates" element={<Templates />} />
+        <Route path="historico" element={<HistoricoDisparoPage />} />
+        <Route path="clientesVencidos" element={<ClientesVencidos/>} />
+        <Route path="CreateTemplate" element={<CreateTemplate />} />
+        <Route path="Campanhas" element={<Campanhas />} />
+
+      </Route>
+
     </Routes>
   );
 }
